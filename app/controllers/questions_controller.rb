f5ba81f08a3d2be3e371dbd569d7e5ddcf388bde
VvDPzZ <vvdpzz@gmail.com>
2011-08-01 10:32:39 +0800

    before develop

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,11 +8,18 @@ class QuestionsController < ApplicationController
   end
   
   def create
-    
+    question = Question.new(params[:question])
+    question.markdown = Helper.markdown(question.content)
+    uuid = UUIDTools::UUID.random_create.to_s.gsub('-','').hex
+    question.id = uuid
+    $redis.set("questions:#{uuid}", question.to_json)
+    redirect_to "/questions/#{uuid}"
   end
   
   def show
-    
+    @question = $redis.get("questions:#{params[:id]}")
+    @question = JSON.parse(@question)
+    puts @question[:markdown]
   end
   
   def edit
